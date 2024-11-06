1b27d09783fc7e33a8161e50da81f654dedf1f43
toxeeec <bartosz.kapciak@gmail.com>
2024-11-06 02:37:41 +0100

    fix completion

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -17,47 +17,58 @@ return {
 			cmp_autopairs.on_confirm_done()
 		)
 
+		local luasnip = require("luasnip")
+
 		cmp.setup({
 			mapping = cmp.mapping.preset.insert({
 				["<C-u>"] = cmp.mapping.scroll_docs(-4),
 				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				['<Tab>'] = cmp.mapping(function(fallback)
-					local luasnip = require('luasnip')
-					local col = vim.fn.col('.') - 1
-
+				['<CR>'] = cmp.mapping(function(fallback)
 					if cmp.visible() then
-						cmp.select_next_item({ behavior = 'select' })
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-						fallback()
+						if luasnip.expandable() then
+							luasnip.expand()
+						else
+							cmp.confirm({
+								select = true,
+							})
+						end
 					else
-						cmp.complete()
+						fallback()
 					end
-				end, { 'i', 's' }),
+				end),
 
-				-- Super shift tab
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					local luasnip = require('luasnip')
-
+				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
-						cmp.select_prev_item({ behavior = 'select' })
+						cmp.select_next_item()
+					elseif luasnip.locally_jumpable(1) then
+						luasnip.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
 					elseif luasnip.locally_jumpable(-1) then
 						luasnip.jump(-1)
 					else
 						fallback()
 					end
-				end, { 'i', 's' }),
+				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' }, -- For luasnip users.
+				-- { name = 'luasnip' }, -- For luasnip users.
 			}, {
 				{ name = 'buffer' },
-			})
+			}),
+			preselect = 'item',
+			completion = {
+				completeopt = 'menu,menuone,noinsert'
+			}
 		})
 
 		cmp.setup.cmdline({ "/", "?" }, {
