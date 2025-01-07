77132bad1040d783b953203a145c51b91467aeec
toxeeec <bartosz.kapciak@gmail.com>
2025-01-07 14:29:34 +0100

    add oil.nvim

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent directory" } },
+		lazy = false,
+		opts = {},
+	}
+}
