54871509e48c05e032c92af45b1d65c151712203
toxeeec <bartosz.kapciak@gmail.com>
2025-01-09 00:19:31 +0100

    add todo-comments.nvim

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -3,4 +3,9 @@ return {
 		"tpope/vim-sleuth",
 		event = { "BufReadPost", "BufNewFile" },
 	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 }
