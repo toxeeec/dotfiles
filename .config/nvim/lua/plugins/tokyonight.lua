0b1dabdccebb8085241aff40e91348418f80a898
toxeeec <bartosz.kapciak@gmail.com>
2025-01-09 00:15:15 +0100

    disable italics

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -4,6 +4,13 @@ return {
 		init = function()
 			vim.cmd.colorscheme("tokyonight-night")
 		end,
+		opts = {
+			style = "night",
+			styles = {
+				comments = { italic = false },
+				keywords = { italic = false },
+			},
+		},
 		priority = 1000,
 	},
 }
