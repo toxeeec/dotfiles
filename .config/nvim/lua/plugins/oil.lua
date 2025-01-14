c0cb06b480cdfa2cda6e6d578e48d8eab1fa0d91
toxeeec <bartosz.kapciak@gmail.com>
2025-01-15 00:55:17 +0100

    open oil in floating window

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"stevearc/oil.nvim",
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent directory" } },
+		keys = { { "-", "<cmd>Oil --float<cr>", desc = "Open parent directory" } },
 		lazy = false,
 		---@module "oil"
 		---@type oil.SetupOpts
