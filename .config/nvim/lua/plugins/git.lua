b109e169cd09482a4db267837812a591deb5e380
toxeeec <bartosz.kapciak@gmail.com>
2025-01-23 02:04:52 +0100

    add neogit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"ibhagwan/fzf-lua",
+		},
+		config = true,
+	},
+	{
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
 		opts = {
