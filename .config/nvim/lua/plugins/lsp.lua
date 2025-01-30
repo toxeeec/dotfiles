272e22118dd4ad881a47f89d247991ef422eab8c
toxeeec <bartosz.kapciak@gmail.com>
2025-01-30 01:22:52 +0100

    update lazydev config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -101,9 +101,8 @@ return {
 		ft = "lua",
 		opts = {
 			library = {
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			},
 		},
 	},
-	{ "Bilal2453/luvit-meta", lazy = true },
 }
